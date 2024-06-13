6ef45e03b53bdf7bd30ea27c73988121cefd8fa3
Nic <3624025+nicdwilson@users.noreply.github.com>
2024-06-14 10:42:52 +1200

    Update retry-failed-woocommerce-webhooks.php

--- retry-failed-woocommerce-webhooks.php
+++ retry-failed-woocommerce-webhooks.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Retry Failed WooCommerce Webhooks
-Description: This plugin rescheduled failed webhooks to retry if a 2xx response code is not received. Webhooks are disabled after 5 retries by default if the delivery URL returns an unsuccessful status such as 404 or 5xx. Successful responses are 2xx, 301 or 302. To increase the number of retries, you can use the woocommerce_max_webhook_delivery_failures filter function. Failers are logged in the webhook-delivery-failure log.
+Description: This plugin reschedules failed webhooks to retry if a 2xx response code is not received. Webhooks are disabled after 5 retries by default if the delivery URL returns an unsuccessful status such as 404 or 5xx. Successful responses are 2xx, 301 or 302. To increase the number of retries, you can use the woocommerce_max_webhook_delivery_failures filter function. Failers are logged in the webhook-delivery-failure log.
 Version: 1.0
 Author: Woo Growth Team
 Domain: retry-failed-webhooks
